6d079ef68d1a495ba47a333baffbf068de29a412
Tyler Chase <tylercchase@gmail.com>
2020-07-13 02:54:45 -0800

    Imports for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-
+import {  } from "./models/user.model";
+import { ViewComponent } from './view/view.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    ViewComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ZXingScannerModule
+    ZXingScannerModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { ZXingScannerComponent } from '@zxing/ngx-scanner';
+import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,14 +11,24 @@ import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 export class RegisterComponent implements OnInit {
   @ViewChild('scanner', { static: false })
   scanner: ZXingScannerComponent;
-  public startScan = false;
-
-  constructor() { }
+  public startScan: Boolean = false;
   
+  cardDetails = new FormGroup({
+    mundane_name: new FormControl('',[Validators.required]),
+    persona_name: new FormControl('',[Validators.required]),
+    notes: new FormControl('',[])
+  })
+  constructor() { }
+
   ngOnInit(): void {
+
   }
-  completeScan(event){
+  public completeScan(event){
     console.log(event);
+    this.startScan = false;
+  }
+  public onSubmit(){
+    this.startScan = true;
   }
 
 }
