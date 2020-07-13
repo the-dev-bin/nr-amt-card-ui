3f0a24450a480d451e2dfa7218e04d194d0eab8b
Tyler Chase <tylercchase@gmail.com>
2020-07-13 04:06:27 -0800

    View user that is scanned

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AmtcardAPIService } from "../services/amtcard-api.service";
+import { User } from '../models/user.model';
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
-  public startScan : Boolean = true;
-  constructor() { }
-
+  public startScan: Boolean = true;
+  constructor(public amtcardAPI: AmtcardAPIService) { }
+  public userInfo: User;
   ngOnInit(): void {
   }
-
+  public completeScan(event){
+    console.log(event);
+    this.startScan = false;
+    this.amtcardAPI.getUser(event).subscribe((user) => this.userInfo = user);
+  }
 }
