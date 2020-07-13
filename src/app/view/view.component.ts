020d43555ea0600a3a264c1edaf91a6cf4ca81d7
Tyler Chase <tylercchase@gmail.com>
2020-07-13 02:54:30 -0800

    Create component for viewing card details

---
+++ src/app/view/view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.scss']
+})
+export class ViewComponent implements OnInit {
+  public startScan : Boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
